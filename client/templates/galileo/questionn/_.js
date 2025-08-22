881541801d807094358887630215f0313de17053
Shreeman5 <shreeman.p.gautam@gmail.com>
2025-08-22 16:33:13 -0600

    small changes in js file

--- client/templates/galileo/questionn/_.js
+++ client/templates/galileo/questionn/_.js
@@ -37,7 +37,7 @@ Template.gaQuestionsn.events({
         // Navigate after short delay
         setTimeout(() => {
           FlowRouter.go('/galileo/visualizationn');
-        }, 1000);
+        }, 500);
       }
     });
   }
